e816984b40aa06c334693cc2bc1e7a0db48c51e6
rkojima <kojima.robert@gmail.com>
2017-02-02 13:35:27 -0800

    kinda stuck, not sure how to utilize youtube api

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search ';
+var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 function getDataFromApi(searchTerm, callback) {
-    var query = {
-        s: searchTerm,
-        r: 'json'
-    };
+    var query = gapi.client.youtube.search.list({
+        q: searchTerm,
+        part: 'snippet',
+        key: 'AIzaSyD1oq6ahRUvelQP2N5bv8JzbWovxQNrhY0'
+    });
     $.getJSON(YOUTUBE_BASE_URL, query, callback);
 }
+
+function displayYoutubeSearchData(data) {
+    var resultElement = '';
+    if (data.Search) {
+        data.Search.forEach(function(item) {
+            resultElement += '<p>' + item.Title + '</p>';
+        });
+    }
+    else {
+        resultElement += '<p>No results</p>';
+    }
+
+}
+
+function watchSubmit() {
+    $('.search-form').submit(function(e) {
+        e.preventDefault();
+        var query = $(this).find('.query').val();
+        getDataFromApi(query, displayYoutubeSearchData);
+    });
+}
+
+$(function() {watchSubmit();});
